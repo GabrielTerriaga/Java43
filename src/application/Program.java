62403c3f41d629618fd995b49d44a347bb8b37db
Gabriel Terriaga <Gmterriaga0@yahoo.com.br>
2019-07-20 23:25:01 -0300

    Aula map

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Map<String, String> cookies = new TreeMap<>();
+		
+		String e = "Gabriel";
+		
+		cookies.put("username", "maria");
+		cookies.put("email", "maria@gmail.com");
+		cookies.put("phone " + e, "99771122");
+		cookies.remove("email");
+		cookies.put("phone " + e, "99771133");
+		
+		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+		System.out.println("Phone number : " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email"));
+		System.out.println("Size: " + cookies.size());
+		System.out.println("ALL COOKIES:");
+		
+		for (String key : cookies.keySet()) {
+			System.out.println(key + ": " + cookies.get(key));
+		}
+
+	}
+
+}
